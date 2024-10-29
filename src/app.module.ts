53333fa5e15ce3b89f083fb7b822563aba41800b
Oleksandr Hembaruk <overmind@Mac-mini-Oleksandr.local>
2024-10-29 19:35:08 +0200

    add env example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TelegrafModule } from "nestjs-telegraf";
+import { TelegrafModule } from 'nestjs-telegraf';
 import { BotModule } from './bot/bot.module';
-import 'dotenv/config'
-import { AppUpdate } from "./app.update";
-
+import 'dotenv/config';
+import { AppUpdate } from './app.update';
 
 @Module({
-  imports: [TelegrafModule.forRoot({
-    token: process.env.TOKEN,
-    launchOptions: {
-      webhook: { domain: process.env.DOMANE, path: '/telegram/webhook' }
-    }
-  }), BotModule],
+  imports: [
+    TelegrafModule.forRoot({
+      token: process.env.TOKEN,
+      launchOptions: {
+        webhook: { domain: process.env.DOMAIN, path: '/telegram/webhook' },
+      },
+    }),
+    BotModule,
+  ],
   controllers: [AppController],
   providers: [AppService, AppUpdate],
 })
